78772711509eeed6abcd9191fded939822b53716
valihan <ilyasovvalihan@gmail.com>
2022-09-05 12:36:36 +0300

    first commit

--- Client.py
+++ Client.py
@@ -3,17 +3,38 @@ import os
 
 client_socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 port = 12300
-hostname = '127.0.0.1'
-buffsize = 1024
+hostname = socket.gethostname()
+buff_size = 1024
 address = (hostname, port)
-seqno = 0
-filename = 'some.txt'
-filesize = os.path.getsize(filename)
-start_message = f"s | {filename} | {filesize}"
+start_seqno = 0
+fileName = 'some.txt'
+filePath = './Client/'+fileName
+filesize = os.path.getsize(filePath)
+start_message = f"s | {start_seqno} | {fileName} | {filesize}"
+print("Start send message")
+#  a | next_seqno | buf_size
+#  a | next_seqno
 client_socket.sendto(start_message.encode(), address)
-answer_from_server, serverAddress = client_socket.recvfrom(buffsize)
-print(answer_from_server.decode())
+answer_from_server, serverAddress = client_socket.recvfrom(buff_size)
+answer_from_server = answer_from_server.decode()
+answer_massage = answer_from_server.split(' | ')
+active = answer_massage[0]
+seqno = int(answer_massage[1])
+if len(answer_massage) == 3:
+    buff_size = int(answer_massage[2])
+with open(filePath, 'r', encoding='utf-8') as f:
+    for i in range(0, filesize, buff_size):
+        print(f.tell())
+        if i != 0:
+            answer_from_server, serverAddress = client_socket.recvfrom(buff_size)
+            answer_from_server = answer_from_server.decode()
+            answer_massage = answer_from_server.split(' | ')
+            active = answer_massage[0]
+        data_chunks = f.read(buff_size)
+        message = f"d | {seqno + 1} | " + data_chunks
+        client_socket.sendto(message.encode(), serverAddress)
 client_socket.close()
+
 
 
 
--- Server.py
+++ Server.py
@@ -5,10 +5,31 @@ socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 port = 12300
 buff_size = 1024
 socket.bind(('', port))
-print("The server is ready to recieve")
+address_file = dict()
+print("The server is ready to receive")
+# s | seqno | filename | total_size
+# d | seqno | data_bytes
 while True:
     message_from_client, address = socket.recvfrom(buff_size)
     message_from_client = message_from_client.decode()
     message = message_from_client.split(' | ')
-    print(message)
+    active = message[0]
+    if active == 's':
+        seqno = int(message[1])
+        filename = message[2]
+        filepath = './Server/' + filename
+        total_size = message[3]
+        address_file[address] = filepath
+        with open(filepath, 'w', encoding='utf-8') as f:
+            f.write('')
+        message_to_client = f'a | {seqno + 1} | {buff_size}'.encode()
+        socket.sendto(message_to_client, address)
+    elif active == 'd':
+        seqno = int(message[1])
+        data_chunks = message[2]
+        filepath = address_file[address]
+        with open(filepath, 'a', encoding='utf-8') as f:
+            f.write(data_chunks)
+        message_to_client = f'a | {seqno + 1} | {buff_size}'.encode()
+        socket.sendto(message_to_client, address)
 
